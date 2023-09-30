e04a050c44e1830994f27e6568c387cd7eab23aa
Tan Le <tanlethanh8266@gmail.com>
2023-09-30 23:46:45 +0700

    add chat packages

---
+++ packages/chat/features/ChatBox/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ChatBox = () => {
+	return (
+		<View>
+			<Text>index</Text>
+		</View>
+	);
+};
+
+export default ChatBox;
+
+const styles = StyleSheet.create({});
