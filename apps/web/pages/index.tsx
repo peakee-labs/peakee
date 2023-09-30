71dffec9b5af29ad1968477da4992658a6feafb5
Tan Le <tanlethanh8266@gmail.com>
2023-09-30 15:28:47 +0700

    use nextjs page router to prevent calling context from Server side

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
+import { Text } from 'react-native';
 import { Button, Header } from '@peakee/ui';
 
 export default function Page(): JSX.Element {
 	return (
 		<>
 			<Header text="Web" />
-			<Button />
+			<Button style={{ width: 100 }} title="Hello web" />
+			<Text>Hello from new text 2</Text>
 		</>
 	);
 }
