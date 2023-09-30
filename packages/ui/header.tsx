657f6dc5526905b7abf4e12711e70e52cb2cf6a7
Tan Le <tanlethanh8266@gmail.com>
2023-09-30 12:58:40 +0700

    format space

--- packages/ui/header.tsx
+++ packages/ui/header.tsx
@@ -1,8 +1,9 @@
 import type { FC } from 'react';
+import { Text } from 'react-native';
 
 interface Props {
 	text: string;
 }
 export const Header: FC<Props> = ({ text }) => {
-	return <h1>{text}</h1>;
+	return <Text>{text}</Text>;
 };
