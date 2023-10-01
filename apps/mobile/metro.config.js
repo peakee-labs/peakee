f02de6f26f0459a1dbb72fb2bbe92c20b9808944
Tan Le <tanlethanh8266@gmail.com>
2023-10-01 23:57:16 +0700

    resolve local modules for mobile

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 
+const monoPackages = {
+	'@peakee/chat': path.resolve(__dirname, '../../packages/chat'),
+	'@peakee/ui': path.resolve(__dirname, '../../packages/ui'),
+};
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -8,7 +13,13 @@ const path = require('path');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-	watchFolders: [path.resolve(__dirname, '../../node_modules')],
+	watchFolders: [
+		path.resolve(__dirname, '../../node_modules'),
+		...Object.values(monoPackages),
+	],
+	resolver: {
+		extraNodeModules: monoPackages,
+	},
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
