01e7c145f4e0efb1917599026321257c535bad85
Tan Le <tanlethanh8266@gmail.com>
2023-10-04 10:29:43 +0700

    feat: add redux toolkit

--- apps/mobile/src/index.tsx
+++ apps/mobile/src/index.tsx
@@ -1,37 +1,40 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ChatRoom from './screens/ChatRoom';
 import Home from './screens/Home';
 import SignIn from './screens/SignIn';
+import { store } from './state/store';
 
 const Stack = createNativeStackNavigator();
 
 export function App(): JSX.Element {
 	return (
-		<NavigationContainer>
-			<SafeAreaView style={styles.app}>
-				<Stack.Navigator>
-					<Stack.Screen
-						name="SignIn"
-						component={SignIn}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="Home"
-						component={Home}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="ChatRoom"
-						component={ChatRoom}
-						options={{ headerShown: false }}
-					/>
-				</Stack.Navigator>
-			</SafeAreaView>
-		</NavigationContainer>
+		<Provider store={store}>
+			<NavigationContainer>
+				<SafeAreaView style={styles.app}>
+					<Stack.Navigator>
+						<Stack.Screen
+							name="SignIn"
+							component={SignIn}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Home"
+							component={Home}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="ChatRoom"
+							component={ChatRoom}
+							options={{ headerShown: false }}
+						/>
+					</Stack.Navigator>
+				</SafeAreaView>
+			</NavigationContainer>
+		</Provider>
 	);
 }
 
