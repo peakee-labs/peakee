b4eb05fa4a2e25f1aa3c890a8b07e9d0c14ab42a
Tan Le <tanlethanh8266@gmail.com>
2023-10-05 02:35:49 +0700

    feat: add friends block

--- apps/mobile/src/screens/Home/components/Friends.tsx
+++ apps/mobile/src/screens/Home/components/Friends.tsx
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import type { FC } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import type { UserChatData } from '@peakee/app/types';
 
-export const Friends = () => {
+interface Props {
+	profiles: UserChatData[];
+}
+
+export const Friends: FC<Props> = ({ profiles }) => {
 	return (
 		<View style={styles.container}>
-			<Text>Friend 1</Text>
-			<Text>Friend 2</Text>
+			{profiles.map((p, i) => {
+				return (
+					<View key={i} style={styles.profileContainer}>
+						<Image
+							style={styles.avatar}
+							source={{ uri: p.imageUrl }}
+						/>
+						<Text>{p.name}</Text>
+					</View>
+				);
+			})}
 		</View>
 	);
 };
@@ -14,5 +29,14 @@ export default Friends;
 const styles = StyleSheet.create({
 	container: {
 		flexDirection: 'row',
+	},
+	profileContainer: {
+		alignItems: 'center',
+	},
+	avatar: {
+		height: 50,
+		width: 50,
+		borderRadius: 25,
+		borderWidth: 0.4,
 	},
 });
