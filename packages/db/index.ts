44e5ca9fa71a739b07899e9e0853515a1c668e39
Tan Le <tanlethanh8266@gmail.com>
2023-10-18 17:01:33 +0700

    feat: add db ioc methods

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,3 +1,46 @@
-export const helloDB = () => {
-	console.log('Hello from @peakee/db');
+import type {
+	CreateNewChatRoomFunction,
+	CreateNewMessageFunction,
+	CreateNewUserFunction,
+	GetChatRoomsFunction,
+	GetUserByEmailFunction,
+	GetUserByFirebaseUIDFunction,
+	GetUserByIDFunction,
+	GetUsersFunction,
+	UpdateFriendFunction,
+} from './interfaces';
+
+type InjectedFunctions = {
+	createNewUser: CreateNewUserFunction;
+	getUserByID: GetUserByIDFunction;
+	getUserByFirebaseUID: GetUserByFirebaseUIDFunction;
+	getUserByEmail: GetUserByEmailFunction;
+	getUsers: GetUsersFunction;
+	updateFriend: UpdateFriendFunction;
+	createNewChatRoom: CreateNewChatRoomFunction;
+	getChatRooms: GetChatRoomsFunction;
+	createNewMessage: CreateNewMessageFunction;
+};
+
+export * from './interfaces';
+export let createNewUser: CreateNewUserFunction;
+export let getUserByID: GetUserByIDFunction;
+export let getUserByFirebaseUID: GetUserByFirebaseUIDFunction;
+export let getUserByEmail: GetUserByEmailFunction;
+export let getUsers: GetUsersFunction;
+export let updateFriend: UpdateFriendFunction;
+export let createNewChatRoom: CreateNewChatRoomFunction;
+export let getChatRooms: GetChatRoomsFunction;
+export let createNewMessage: CreateNewMessageFunction;
+
+export const injectFirestoreFunctions = (functions: InjectedFunctions) => {
+	console.log('Injecting db functions');
+	createNewUser = functions.createNewUser;
+	getUserByID = functions.getUserByID;
+	getUserByFirebaseUID = functions.getUserByFirebaseUID;
+	getUsers = functions.getUsers;
+	updateFriend = functions.updateFriend;
+	createNewChatRoom = functions.createNewChatRoom;
+	getChatRooms = functions.getChatRooms;
+	createNewMessage = functions.createNewMessage;
 };
---
+++ packages/db/interfaces.ts
@@ -0,0 +1,34 @@
+import type { ChatRoom, Message, UserChatData } from './types';
+
+export type CreateNewUserFunction = (
+	user: Omit<UserChatData, 'id'>,
+) => Promise<UserChatData>;
+
+export type GetUserByIDFunction = (
+	id: string,
+) => Promise<UserChatData | undefined>;
+
+export type GetUserByFirebaseUIDFunction = (
+	uid: string,
+) => Promise<UserChatData | undefined>;
+
+export type GetUserByEmailFunction = (
+	email: string,
+) => Promise<UserChatData | undefined>;
+
+export type GetUsersFunction = (ids: string[]) => Promise<UserChatData[]>;
+
+export type UpdateFriendFunction = (
+	user: UserChatData,
+	friend: UserChatData,
+) => Promise<void>;
+
+export type CreateNewChatRoomFunction = (
+	room: Omit<ChatRoom, 'id'>,
+) => Promise<ChatRoom>;
+
+export type GetChatRoomsFunction = (ids: string[]) => Promise<ChatRoom[]>;
+
+export type CreateNewMessageFunction = (
+	user: Omit<Message, 'id'>,
+) => Promise<Message>;
