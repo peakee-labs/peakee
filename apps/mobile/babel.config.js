b94fa7bc3d457b80ef79326d74c78102753f375b
Tan Le <tanlethanh8266@gmail.com>
2023-10-25 21:29:40 +0700

    feat: mobile alias

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				alias: {
+					assets: './assets',
+					utils: './src/utils',
+					screens: './src/screens',
+				},
+			},
+		],
+	],
 };
