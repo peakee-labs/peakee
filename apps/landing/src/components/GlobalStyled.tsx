6d97aa098a04e054fb17cd7f791e5aa802172b48
Tan Le <tanlethanh8266@gmail.com>
2024-01-06 22:00:24 +0700

    chore: change transition time

--- apps/landing/src/components/GlobalStyled.tsx
+++ apps/landing/src/components/GlobalStyled.tsx
@@ -4,6 +4,6 @@ export const GlobalStyles = createGlobalStyle`
     body {
         background: ${({ theme }) => theme.body};
         color: ${({ theme }) => theme.text};
-        transition: all 0.50s linear;
+        transition: all 0.1s linear;
     }
 `;
