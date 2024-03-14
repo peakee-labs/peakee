d7080ed6181859f9153bf7fb1ffb3f0569d79fcb
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-14 11:36:36 +0700

    feat: progress bar on  onboarding screen

---
+++ apps/mobile/src/screens/onboarding/components/ProgressBar.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const dimension = Dimensions.get('window');
+
+interface ProgressBarProps {
+	current: number;
+	max: number;
+}
+const ProgressBar = ({ current, max }: ProgressBarProps) => {
+	return (
+		<View style={styles.container}>
+			{Array.from({ length: max }).map((_, idx) => {
+				return idx == current ? (
+					<View
+						key={idx}
+						style={{ ...styles.base, ...styles.active }}
+					></View>
+				) : (
+					<View
+						key={idx}
+						style={{ ...styles.base, ...styles.inactive }}
+					></View>
+				);
+			})}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		width: dimension.width * 0.8,
+		alignSelf: 'center',
+		height: 10,
+		display: 'flex',
+		gap: dimension.width * 0.02,
+		flexDirection: 'row',
+	},
+	base: {
+		flex: 1,
+		height: 10,
+		borderColor: '#000000',
+		borderRadius: 10,
+	},
+	active: {
+		backgroundColor: '#FF5A00',
+	},
+	inactive: {
+		backgroundColor: '#E3E3E3',
+	},
+});
+export default ProgressBar;
