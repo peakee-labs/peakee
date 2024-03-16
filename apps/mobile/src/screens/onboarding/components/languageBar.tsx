9bc68b1a110f70f549b6e64db2883f5e7d7ef816
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-16 13:34:34 +0700

    feat: onboarding language-related screens

---
+++ apps/mobile/src/screens/onboarding/components/languageBar.tsx
@@ -0,0 +1,57 @@
+import { Dimensions, Pressable, StyleSheet, Text, View } from 'react-native';
+import CountryFlag from 'react-native-country-flag';
+
+interface LanguageBarProps {
+	isoCode: string; // ISO-3166 format
+	name: string;
+	onPress: () => void;
+	isActive: boolean;
+}
+const dimension = Dimensions.get('window');
+
+const LanguageBar = ({
+	isActive,
+	onPress,
+	isoCode,
+	name,
+}: LanguageBarProps) => {
+	let containerStyle;
+	if (isActive) {
+		containerStyle = { ...styles.container, ...styles.active };
+	} else {
+		containerStyle = styles.container;
+	}
+
+	return (
+		<Pressable onPress={onPress} style={containerStyle}>
+			<CountryFlag isoCode={isoCode} size={30} style={styles.flag} />
+			<Text style={styles.name}>{name}</Text>
+		</Pressable>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		width: dimension.width * 0.9,
+		alignSelf: 'center',
+		height: 50,
+		borderColor: '#000000',
+		borderRadius: 10,
+		borderWidth: 1,
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		paddingHorizontal: 10,
+		gap: 10,
+	},
+	active: {
+		borderColor: '#FF7670',
+	},
+	flag: {},
+	name: {
+		textTransform: 'capitalize',
+		fontSize: 17,
+	},
+});
+
+export default LanguageBar;
---
+++ apps/mobile/src/screens/onboarding/language/index.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { err } from 'react-native-svg';
+import { useNavigation } from '@react-navigation/native';
+
+import { NavigateBar } from '../components';
+import LanguageBar from '../components/languageBar';
+import type { OnboardingValue } from '../store';
+import { FormState } from '../store';
+const mock: Array<{ isoCode: string; name: string }> = [
+	{ isoCode: 'vn', name: 'vietnamese' },
+	{ isoCode: 'gb', name: 'english' },
+];
+
+const OnboardingLanguage = () => {
+	const [active, setActive] = useState<number>(-1);
+	const [error, setError] = useState<Error | undefined>();
+	const navigation = useNavigation();
+
+	const onSubmit = (native?: string) => () => {
+		if (native) {
+			console.log(native);
+			FormState.update((s: OnboardingValue) => {
+				s.progress += 1;
+				s.native = native;
+			});
+			navigation.navigate('OnboardingStep4' as never);
+		} else {
+			setError({
+				name: 'language',
+				message: "Let' tell me your native",
+			});
+		}
+	};
+	const toggleActive = (idx: number) => () => {
+		setActive((curr) => (curr == idx ? -1 : idx));
+		setError(undefined);
+	};
+
+	const onBack = () => {
+		FormState.update((s: OnboardingValue) => {
+			s.progress -= 1;
+		});
+		navigation.goBack();
+	};
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>My native language is...</Text>
+			<View style={styles.languageList}>
+				{mock.map((item, idx) => (
+					<LanguageBar
+						key={idx}
+						isoCode={item.isoCode}
+						name={item.name}
+						onPress={toggleActive(idx)}
+						isActive={idx == active}
+					/>
+				))}
+			</View>
+			{error ? <Text>{error.message}</Text> : undefined}
+			<View>
+				<NavigateBar
+					onPrev={onBack}
+					onNext={onSubmit(mock[active]?.name)}
+				/>
+			</View>
+		</View>
+	);
+};
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 20,
+		paddingVertical: 20,
+		gap: 30,
+	},
+	languageList: {
+		flexDirection: 'column',
+		gap: 10,
+		flex: 1,
+	},
+	title: {
+		color: '#000000',
+		fontSize: 27,
+	},
+	footer: {},
+});
+export default OnboardingLanguage;
