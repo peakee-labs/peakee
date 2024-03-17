a9f11110ac558192499f19ecd44476926cae6769
Tan Le <tanlethanh8266@gmail.com>
2024-03-17 14:00:28 +0700

    feat: init websocket connection

---
+++ packages/app/websocket/index.ts
@@ -0,0 +1,35 @@
+export function initWebsocket(endpoint: string, token: string) {
+	const ws = new WebSocket(`${endpoint}?token=${token}`);
+
+	ws.onmessage = (event) => {
+		console.log(event.data);
+	};
+
+	ws.onopen = () => {
+		console.log('Connected to websocket');
+	};
+
+	return ws;
+}
+
+enum WS_TYPE {
+	DEFAULT,
+}
+
+export const wsMap: Map<WS_TYPE, { ws: WebSocket; id: string }> = new Map();
+
+export const initWebsocketWithProfile = (
+	endpoint: string,
+	userId: string,
+	jwt: string,
+) => {
+	if (wsMap.has(WS_TYPE.DEFAULT)) {
+		wsMap.get(WS_TYPE.DEFAULT)?.ws.close();
+		wsMap.delete(WS_TYPE.DEFAULT);
+	}
+
+	wsMap.set(WS_TYPE.DEFAULT, {
+		ws: initWebsocket(endpoint, jwt),
+		id: userId,
+	});
+};
