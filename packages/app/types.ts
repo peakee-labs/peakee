1496f40a241cea59891a6a19e4fd9cdc66b776ee
Tan Le <tanlethanh8266@gmail.com>
2024-03-18 11:47:28 +0700

    feat: add chat types

--- packages/app/types.ts
+++ packages/app/types.ts
@@ -1,4 +1,4 @@
-export interface UserProfile {
+export type UserProfile = {
 	id: string;
 	name: string;
 	email: string;
@@ -6,16 +6,16 @@ export interface UserProfile {
 	createdAt: string;
 	updatedAt: string;
 	friends: string[];
-}
+};
 
-export interface PublicUserProfile {
+export type PublicUserProfile = {
 	id: string;
 	name: string;
 	email: string;
 	imageURL: string;
-}
+};
 
-export interface FriendRequest {
+export type FriendRequest = {
 	id: string;
 	from: string;
 	to: string;
@@ -23,4 +23,40 @@ export interface FriendRequest {
 	createdAt: string;
 	updatedAt: string;
 	user?: PublicUserProfile;
-}
+};
+
+export type Conversation = {
+	id: string;
+	type: 'individual' | 'group';
+	members: [
+		{
+			userId: string;
+			createdAt: string;
+			updatedAt: string;
+			joinAt: string;
+		},
+	];
+	createdBy: string;
+	createdAt: string;
+	updatedAt: string;
+	messages?: Message[];
+};
+
+export type Message = {
+	id: string;
+	senderId: string;
+	conversationId: string;
+	replyTo: string;
+	content: string;
+	status: 'pending' | 'sent' | 'delivered' | 'read';
+	createdAt: string;
+	updatedAt: string;
+	emotions: MessageEmotion[];
+};
+
+export type MessageEmotion = {
+	senderId: string;
+	content: string;
+	createdAt: string;
+	updatedAt: string;
+};
