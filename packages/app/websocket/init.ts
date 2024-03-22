ee63b29d83de48ec34a051b9f293928dfe7ba2c3
Tan Le <tanlethanh8266@gmail.com>
2024-03-22 14:43:50 +0700

    fix: query conversation at the first message received

--- packages/app/websocket/init.ts
+++ packages/app/websocket/init.ts
@@ -4,7 +4,7 @@ import {
 	store,
 	updateLatestMessage,
 } from '../state';
-import { config } from '../utils';
+import { config, getConversationWithState } from '../utils';
 
 import type { AckSendMessagePayload, NewMessagePayload } from './events';
 import { type EventPayload, EventType } from './events';
@@ -12,7 +12,7 @@ import { type EventPayload, EventType } from './events';
 function initWebsocket(endpoint: string, token: string) {
 	const ws = new WebSocket(`${endpoint}?token=${token}`);
 
-	ws.onmessage = (event) => {
+	ws.onmessage = async (event) => {
 		const data = JSON.parse(event.data) as EventPayload;
 		console.log('Received message: ', data.type);
 		if (data.type === EventType.SERVER_ACK_SEND_MESSAGE) {
@@ -27,6 +27,14 @@ function initWebsocket(endpoint: string, token: string) {
 			);
 		} else if (data.type === EventType.SERVER_SEND_MESSAGE) {
 			const payload = data as NewMessagePayload;
+			const conversation =
+				store.getState().chat.conversationsMap[
+					payload.message.conversationId
+				];
+			if (!conversation) {
+				await getConversationWithState(payload.message.conversationId);
+			}
+
 			store.dispatch(
 				addMessage({
 					conversationId: payload.message.conversationId,
