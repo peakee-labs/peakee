2e8c054094cc8333f24919cf2adc8a5135e8bff7
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-24 20:39:41 +0700

    feat: load assets

--- apps/mobile/src/utils/bootstrap.ts
+++ apps/mobile/src/utils/bootstrap.ts
@@ -11,6 +11,10 @@ export const initApp = () => {
 	initAssets({
 		authImage: { uri: '/images/auth.png' },
 		google: { uri: '/images/google.png' },
+		message: { uri: '/images/onboarding-start.png' },
+		messageStack: { uri: '/images/onboarding-message.png' },
+		messagePuzzle: { uri: '/images/onboarding-messagePuzzle.png' },
+		background: { uri: 'onboarding-background.png' },
 	});
 
 	initAppConfig({
