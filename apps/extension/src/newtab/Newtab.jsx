7e1aa3b08b69e9bd8a7508a0d3534fb38812113e
Tan Le <tanlethanh8266@gmail.com>
2024-03-29 00:39:30 +0700

    feat: ext with translation

--- apps/extension/src/newtab/Newtab.jsx
+++ apps/extension/src/newtab/Newtab.jsx
@@ -1,13 +1,62 @@
-import { Button } from '@peakee/ui';
+import { StyleSheet, Text, View } from 'react-native';
+import { TranslateBox } from '@peakee/app/features/TranslateBox';
 
 const Newtab = () => {
 	return (
-		<div>
-			<img src={'/logo.svg'} alt="logo" width={100} />
-			<p>Hello Peakee</p>
-			<Button title="Hello world" />
-		</div>
+		<View style={styles.container}>
+			<Text style={styles.header}>
+				Peakee Tools | Learn and use English everywhere
+			</Text>
+
+			<View style={styles.contentContainer}>
+				<TranslateBox style={styles.translateContainer} />
+				<View style={styles.reviewContainer}>
+					<Text style={styles.reviewText}>Metonymy</Text>
+					<Text style={styles.explainText}>
+						the act of referring to something by the name of
+						something else that is closely connected with it
+					</Text>
+				</View>
+			</View>
+		</View>
 	);
 };
 
 export default Newtab;
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+	},
+	header: {
+		fontSize: 18,
+		fontWeight: '500',
+	},
+	contentContainer: {
+		flexDirection: 'row',
+		marginTop: 30,
+	},
+	translateContainer: {
+		width: 500,
+		borderWidth: 1,
+		borderRadius: 20,
+		padding: 20,
+	},
+	reviewContainer: {
+		flex: 1,
+		alignItems: 'center',
+		gap: 20,
+	},
+	reviewText: {
+		fontSize: 100,
+		fontWeight: '600',
+		textAlign: 'center',
+	},
+	explainText: {
+		fontSize: 20,
+		fontStyle: 'italic',
+		fontWeight: '300',
+		color: '#636363',
+		textAlign: 'center',
+	},
+});
--- apps/extension/src/popup/Popup.jsx
+++ apps/extension/src/popup/Popup.jsx
@@ -1,9 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { TranslateBox } from '@peakee/app/features/TranslateBox';
+
 const Popup = () => {
 	return (
-		<div className="App">
-			<h1>Hello world</h1>
-		</div>
+		<View style={styles.container}>
+			<Text style={styles.header}>Peakee Tools</Text>
+			<TranslateBox style={styles.translateContainer} />
+		</View>
 	);
 };
 
 export default Popup;
+
+const styles = StyleSheet.create({
+	container: {
+		width: 500,
+		height: 600,
+		paddingVertical: 6,
+		paddingHorizontal: 10,
+		borderRadius: 10,
+		gap: 10,
+	},
+	header: {
+		fontSize: 20,
+		fontWeight: '600',
+	},
+	translateContainer: {
+		borderWidth: 1,
+		borderRadius: 20,
+		padding: 10,
+	},
+});
