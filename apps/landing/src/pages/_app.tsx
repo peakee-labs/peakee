a4c8717935a34f4fdc3942a8482ed5abee8fe35d
Tan Le <tanlethanh8266@gmail.com>
2024-03-30 20:14:36 +0700

    ui: apply layout to root of each page

--- apps/landing/src/pages/_app.tsx
+++ apps/landing/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type FC } from 'react';
 import type { AppProps } from 'next/app';
+import { Montserrat } from 'next/font/google';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from '../components/GlobalStyled';
@@ -8,13 +9,19 @@ import { useThemeMode } from '../utils';
 
 import '../styles/globals.css';
 
+const montserrat = Montserrat({
+	subsets: ['latin'],
+});
+
 export const App: FC<AppProps> = ({ Component, pageProps }) => {
 	const { themeMode } = useThemeMode();
 
 	return (
 		<ThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
 			<GlobalStyles />
-			<Component {...pageProps} />
+			<div className={`${montserrat.className}`}>
+				<Component {...pageProps} />
+			</div>
 		</ThemeProvider>
 	);
 };
--- apps/landing/src/pages/index.tsx
+++ apps/landing/src/pages/index.tsx
@@ -1,26 +1,16 @@
-import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
-import styled from 'styled-components';
 
+import { Container } from '@/components';
 import { Header, MainSection } from '@/features/HomePage';
-
-const montserrat = Montserrat({
-	subsets: ['latin'],
-});
 
 export default function Home() {
 	return (
-		<Container className={`${montserrat.className}`}>
+		<Container>
 			<Head>
 				<title>Peakee | Learn English by Chatting</title>
 			</Head>
-
 			<Header />
 			<MainSection />
 		</Container>
 	);
 }
-
-const Container = styled.div`
-	width: 100%;
-`;
