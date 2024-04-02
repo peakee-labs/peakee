468eadbc86017f58033d5c11c98d031439a5487c
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-03 01:23:24 +0700

    feat: useLocale as hook to localize content

---
+++ apps/extension/src/utils/hooks/useLocale.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import type { locale } from '../../types/types';
+
+function useLocaleMap<TypeValue>(
+	localeMap: Record<locale, Record<string, TypeValue>>,
+	locale: locale,
+	defaultLocale: locale,
+) {
+	const [currLocaleMap, setLocaleMap] = useState(
+		localeMap[locale] || localeMap[defaultLocale],
+	);
+	const changeLocale = (locale: locale) => {
+		if (!(locale in localeMap)) {
+			console.log('locale', locale, 'not exsited fallback to current');
+		}
+		setLocaleMap(localeMap[locale] || currLocaleMap);
+	};
+
+	const localize = (key: string) => {
+		return currLocaleMap[key];
+	};
+
+	return { changeLocale, localize };
+}
+
+export default useLocaleMap;
