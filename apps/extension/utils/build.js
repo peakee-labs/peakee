63902b490b5e9c085c3447c024544939ab8725b0
Tan Le <tanlethanh8266@gmail.com>
2024-04-18 15:43:28 +0700

    feat: separate environments for dev, staging, production

--- apps/extension/utils/build.js
+++ apps/extension/utils/build.js
@@ -1,5 +1,13 @@
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+const { getMode, loadEnvWithEnvFileFlag } = require('../../../tools/bundler');
+
+loadEnvWithEnvFileFlag();
+
+const mode = getMode() || 'development';
+
+console.log('Building mode:', mode);
+
+process.env.BABEL_ENV = mode;
+process.env.NODE_ENV = mode;
 process.env.ASSET_PATH = '/';
 
 const webpack = require('webpack');
@@ -7,7 +15,7 @@ const path = require('path');
 const config = require('../webpack.config');
 const ZipPlugin = require('zip-webpack-plugin');
 
-config.mode = 'production';
+config.mode = mode;
 
 const packageInfo = require('../package.json');
 
@@ -15,7 +23,7 @@ console.log('Building version:', packageInfo.version);
 
 config.plugins.push(
 	new ZipPlugin({
-		filename: `peakee-ext-${packageInfo.version}.zip`,
+		filename: `peakee-ext-${packageInfo.version}-{mode}.zip`,
 		path: path.join(__dirname, '../build/ext-zip'),
 	}),
 );
