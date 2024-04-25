741a261f155e850d0a2ca92cc7e11c4d215bbc3c
Tan Le <tanlethanh8266@gmail.com>
2024-04-25 13:06:59 +0700

    chore: add loading in withAuth

--- apps/extension/src/utils/withAuth.tsx
+++ apps/extension/src/utils/withAuth.tsx
@@ -1,5 +1,6 @@
 import type { JSX } from 'react';
 import { Fragment } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import SignInFeature from '@peakee/app/features/SignIn';
 import type { RootState } from '@peakee/app/state';
@@ -11,11 +12,23 @@ export const withAuth = (WrappedComponent: () => JSX.Element) => {
 		const userProfile = useSelector(
 			(state: RootState) => state.user.profile,
 		);
+		const profileLoading = useSelector(
+			(state: RootState) => state.user.profileLoading,
+		);
 
 		return (
 			<Fragment>
-				{!userProfile ? (
-					<SignInFeature onPressSignIn={signIn} />
+				{profileLoading ? (
+					<View style={styles.container}>
+						<ActivityIndicator />
+					</View>
+				) : !userProfile ? (
+					<View style={styles.container}>
+						<SignInFeature
+							style={styles.signInBox}
+							onPressSignIn={signIn}
+						/>
+					</View>
 				) : (
 					<WrappedComponent />
 				)}
@@ -27,3 +40,22 @@ export const withAuth = (WrappedComponent: () => JSX.Element) => {
 };
 
 export default withAuth;
+
+const styles = StyleSheet.create({
+	container: {
+		height: '100vh' as never,
+		paddingHorizontal: 20,
+		paddingBottom: 20,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	signInBox: {
+		width: 460,
+		paddingVertical: 60,
+		paddingHorizontal: 40,
+		gap: 100,
+		borderWidth: 1,
+		borderColor: '#B1B6C1',
+		borderRadius: 20,
+	},
+});
