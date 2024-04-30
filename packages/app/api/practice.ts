1825259b282262b2ab1a0010091f89130dbc54ed
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-30 08:47:03 +0700

    feat: use custom for requesting practice unit

--- packages/app/api/practice.ts
+++ packages/app/api/practice.ts
@@ -1,13 +1,10 @@
-import { default as defaultAxios } from 'axios';
-
 import type { reviewWord } from '../types';
-import { config } from '../utils';
 
 import { axios } from './axios';
 
 export async function getRandomPracticeWord(localeCode: string) {
 	try {
-		const { data: word } = await defaultAxios.get<reviewWord>(
+		const { data: word } = await axios().get<reviewWord>(
 			`/practice/unit/random?lang=${localeCode}`,
 		);
 		return word;
@@ -18,10 +15,7 @@ export async function getRandomPracticeWord(localeCode: string) {
 
 export async function getPracticeWordForUser() {
 	try {
-		const { data: word } = await axios().get<reviewWord>(
-			// TODO: deploy practice function
-			`/practice/unit`,
-		);
+		const { data: word } = await axios().get<reviewWord>(`/practice/unit`);
 		return word;
 	} catch (err) {
 		console.log('Error getting practice word', err);
