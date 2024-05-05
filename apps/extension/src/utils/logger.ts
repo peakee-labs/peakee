3bc8240809094de1acbaadfc1d41e8639a54bf49
Tan Le <tanlethanh8266@gmail.com>
2024-05-05 22:17:51 +0700

    chore: remove logger overriding error on ext dev tool

--- apps/extension/src/utils/logger.ts
+++ apps/extension/src/utils/logger.ts
@@ -3,6 +3,5 @@ import type { Logger } from '@peakee/logger';
 export let logger: Logger;
 
 export const setUtilsLogger = (_logger: Logger) => {
-	if (logger) throw Error('Logger already set');
 	logger = _logger;
 };
