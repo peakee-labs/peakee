39003be182c96606a789b16619c8dba5ad510dc0
Tan Le <tanlethanh8266@gmail.com>
2024-05-21 21:50:04 +0700

    chore: rename mobile app to Peakee

--- apps/mobile/ios/PeakeeTests/PeakeeTests.m
+++ apps/mobile/ios/PeakeeTests/PeakeeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileTests : XCTestCase
+@interface PeakeeTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation PeakeeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
