9b3f9b1d9107d469a0a96645cc459923bbe5ff76
Dat Nguyen <minhdat15012002@gmail.com>
2024-05-26 21:29:09 +0700

    feat: fetch user's collections when enter practice space

--- apps/mobile/src/screens/Practice/index.tsx
+++ apps/mobile/src/screens/Practice/index.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { getFlashCardCollectionsInformation } from '@peakee/app/api';
+import type { PracticeFlashCardCollectionInformation } from '@peakee/app/types';
 import DefaultContainer from 'components/DefaultContainer';
 
+import { CollectionPreviews } from './CollectionPreview';
 import Header from './Header';
 import LatestReview from './LatestReview';
 
 export const PracticeScreen = () => {
+	const [collections, setCollections] = useState<
+		PracticeFlashCardCollectionInformation[]
+	>([]);
+
+	useEffect(() => {
+		const fetchCollections = async () => {
+			const collections = await getFlashCardCollectionsInformation();
+			if (collections) {
+				setCollections(collections);
+			}
+		};
+		fetchCollections();
+	}, []);
+
 	return (
 		<DefaultContainer style={styles.container}>
 			<Header />
 			<LatestReview />
+			<CollectionPreviews collections={collections} />
 		</DefaultContainer>
 	);
 };
