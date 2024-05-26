ee4f5db17b718671c2a6bff691ecf33316bdec2d
Dat Nguyen <minhdat15012002@gmail.com>
2024-05-26 21:20:42 +0700

    feat: collections preview in practice screen

---
+++ apps/mobile/src/screens/Practice/Flashcard/Preview.tsx
@@ -0,0 +1,37 @@
+import type { FC } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { FlashcardPreview } from '@peakee/app/features/Practice/FlashcardPreview';
+import type { PracticeFlashCardCollectionInformation } from '@peakee/app/types';
+import { useNavigation } from '@react-navigation/native';
+
+export type Props = {
+	collection: PracticeFlashCardCollectionInformation;
+};
+export const FlashCardPreview: FC<Props> = ({ collection }) => {
+	const { navigate } = useNavigation();
+
+	const handlePressLatestFlashcardReview2 = () => {
+		navigate('PracticeStack', {
+			screen: 'Flashcard',
+			params: { collectionId: collection.id },
+		});
+	};
+
+	return (
+		<View style={styles.container}>
+			<FlashcardPreview
+				name={collection.name}
+				description={collection.description}
+				reviewedCardCount={
+					collection.reviewed ? collection.reviewed.length : 0
+				}
+				totalCardCount={collection.total ? collection.total.length : 0}
+				onPressReview={handlePressLatestFlashcardReview2}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {},
+});
