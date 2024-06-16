87dd54b8384693c328576d9713c062b43ca36ba1
Tan Le <tanlethanh8266@gmail.com>
2024-06-16 09:55:48 +0700

    chore: use Hoverable instead of alum animation

--- apps/extension/src/entries/contentScript/ToolBox/index.tsx
+++ apps/extension/src/entries/contentScript/ToolBox/index.tsx
@@ -1,11 +1,7 @@
 import { type FC } from 'react';
 import type { StyleProp, ViewStyle } from 'react-native';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import Animated, {
-	useAnimatedStyle,
-	useSharedValue,
-	withSpring,
-} from 'react-native-reanimated';
+import { StyleSheet, View } from 'react-native';
+import { Hoverable } from '@peakee/ui';
 
 type Props = {
 	style?: StyleProp<ViewStyle>;
@@ -14,83 +10,23 @@ type Props = {
 	onPressDictionary?: () => void;
 };
 
-type HoverItemType = 'translate' | 'explain' | 'dictionary';
-
 export const ToolBox: FC<Props> = ({
 	style,
 	onPressTranslate,
 	onPressExplain,
 	onPressDictionary,
 }) => {
-	const translateScale = useSharedValue(1);
-	const translateScaleStyle = useAnimatedStyle(() => {
-		return { transform: [{ scale: translateScale.value }] };
-	});
-
-	const explainScale = useSharedValue(1);
-	const explainScaleStyle = useAnimatedStyle(() => {
-		return { transform: [{ scale: explainScale.value }] };
-	});
-
-	const dictionaryScale = useSharedValue(1);
-	const dictionaryScaleStyle = useAnimatedStyle(() => {
-		return { transform: [{ scale: dictionaryScale.value }] };
-	});
-
-	const handleHover = (item: HoverItemType) => {
-		if (item === 'translate') {
-			translateScale.value = withSpring(1.05);
-		} else if (item === 'explain') {
-			explainScale.value = withSpring(1.05);
-		} else if (item === 'dictionary') {
-			dictionaryScale.value = withSpring(1.05);
-		}
-	};
-
-	const handleHoverOut = (item: HoverItemType) => {
-		if (item === 'translate') {
-			translateScale.value = withSpring(1);
-		} else if (item === 'explain') {
-			explainScale.value = withSpring(1);
-		} else if (item === 'dictionary') {
-			dictionaryScale.value = withSpring(1);
-		}
-	};
-
 	return (
 		<View style={[styles.boxContainer, style]}>
-			<TouchableOpacity
-				onPress={onPressTranslate}
-				// @ts-ignore No overload matches this call
-				onMouseEnter={() => handleHover('translate')}
-				onMouseLeave={() => handleHoverOut('translate')}
-			>
-				<Animated.Text style={[styles.title, translateScaleStyle]}>
-					Translate
-				</Animated.Text>
-			</TouchableOpacity>
-
-			<TouchableOpacity
-				onPress={onPressExplain}
-				// @ts-ignore No overload matches this call
-				onMouseEnter={() => handleHover('explain')}
-				onMouseLeave={() => handleHoverOut('explain')}
-			>
-				<Animated.Text style={[styles.title, explainScaleStyle]}>
-					Explain
-				</Animated.Text>
-			</TouchableOpacity>
-
-			<TouchableOpacity
-				onPress={onPressDictionary}
-				// @ts-ignore No overload matches this call
-				onMouseEnter={() => handleHover('dictionary')}
-				onMouseLeave={() => handleHoverOut('dictionary')}
-			>
-				<Animated.Text style={[styles.title, dictionaryScaleStyle]}>
-					Dictionary
-				</Animated.Text>
-			</TouchableOpacity>
+			<Hoverable hoverOpacity={0.5} onPress={onPressTranslate}>
+				Translate
+			</Hoverable>
+			<Hoverable hoverOpacity={0.5} onPress={onPressExplain}>
+				Explain
+			</Hoverable>
+			<Hoverable hoverOpacity={0.5} onPress={onPressDictionary}>
+				Dictionary
+			</Hoverable>
 		</View>
 	);
 };
