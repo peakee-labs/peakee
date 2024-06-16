ad93d55f88bc31ae0ab4293a66088e5628e673ae
Tan Le <tanlethanh8266@gmail.com>
2024-06-16 10:52:32 +0700

    chore: 0.0.7 release with open dictionary

--- apps/landing/src/features/HomePage/MainSection.tsx
+++ apps/landing/src/features/HomePage/MainSection.tsx
@@ -19,7 +19,7 @@ export const MainSection = () => {
 
 	const downloadChromeExtension = () => {
 		window.open(
-			'/peakee-ext-0.0.6-production.zip',
+			'/peakee-ext-0.0.7-production.zip',
 			'_blank',
 			'noopener,noreferrer',
 		);
