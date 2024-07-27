0d180318e8bba41f7cb3525ae1d570eebd24de2d
Tan Le <tanlethanh8266@gmail.com>
2024-07-27 18:13:30 +0700

    feat: content script auto reconnect to kernel

--- apps/extension/src/contentScript/channel.ext.ts
+++ apps/extension/src/contentScript/channel.ext.ts
@@ -1,5 +1,12 @@
 import { ChromeChannel } from '@metacraft/crab/chrome';
+import { logger } from '@peakee/logger';
 
 import { Channels } from '../utils/messaging';
 
-export const channel = new ChromeChannel(Channels.ContentScript);
+export const channel = new ChromeChannel(Channels.ContentScript, {
+	autoReconnect: true,
+});
+
+channel.connection.onDisconnect.addListener(() => {
+	logger().warn('Port disconnected, attempting to reconnect...');
+});
